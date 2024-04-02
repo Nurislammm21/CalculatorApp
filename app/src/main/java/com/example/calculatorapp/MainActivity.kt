837338f60f63fcde5533e28a13f55fc7c9e2f9ab
Nurislam <gggrrraaa1235@gmail.com>
2024-04-02 16:58:03 +0500

    last commit adding functions of btns

--- app/src/main/java/com/example/calculatorapp/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp/MainActivity.kt
@@ -3,14 +3,34 @@ package com.example.calculatorapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.calculatorapp.ui.theme.CalculatorAppTheme
+import com.example.calculatorapp.ui.theme.MediumGrey
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CalculatorAppTheme {
-
+            val viewModel = viewModel<CalculatorViewModel>()
+            val state = viewModel.state
+            val buttonSpacing = 8.dp
+            Calculator(
+                state = state,
+                onAction = viewModel::onAction,
+                buttonSpacing = buttonSpacing,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MediumGrey)
+                    .padding(16.dp)
+                )
 
             }
         }
